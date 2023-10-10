521a26dc92c33dc7c66ca603647a016287b883cd
Pedro Pinto <120741472+pedropintoo@users.noreply.github.com>
2023-10-10 14:52:35 +0100

    Delete P directory

--- P/P1/funcaoComplexa.m
+++
@@ -1,8 +0,0 @@
-
-w = linspace(0, 2 * pi, 200);
-f = sin(4*w).*exp(j*w);
-plot(f);
-xlabel("xx");
-ylabel("yy");
-title("f");
-axis square
--- P/P2/Parte1/potencia_associada.m
+++
@@ -1,8 +0,0 @@
-function p = potencia_associada(x, ts, T)
-    % nº amostras
-    N = T/ts; 
-
-    % media da soma das potencias
-    p = x(1:N)*x(1:N)'/N;   
-
-end
